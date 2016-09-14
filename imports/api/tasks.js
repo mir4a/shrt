6228794ad788cae53147486b3cde2a51c58f131c
Miroslav Martynov <marty.mircha@gmail.com>
2016-09-14 21:44:14 +0300

    Tutorial remove insecure, implement CRUD methods for tasks, users now can toggle/remove only their tasks or common tasks

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -1,3 +1,38 @@
+import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
 import { Mongo } from 'meteor/mongo';
 
 export const Tasks = new Mongo.Collection('tasks');
+
+Meteor.methods({
+  'tasks.insert'(text) {
+    check(text, String);
+
+    // Make sure the user is logged in before inserting a task
+    if (!this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    Tasks.insert({
+      text,
+      createdAt: new Date(),
+      owner: this.userId,
+      username: Meteor.users.findOne(this.userId).username,
+    });
+  },
+  'tasks.remove'(taskId) {
+    check(taskId, String);
+    const tasksOwner = Tasks.findOne(taskId).owner;
+    if (tasksOwner && this.userId !== tasksOwner) return;
+
+    Tasks.remove(taskId);
+  },
+  'tasks.setChecked'(taskId, setChecked) {
+    check(taskId, String);
+    check(setChecked, Boolean);
+    const tasksOwner = Tasks.findOne(taskId).owner;
+    if (tasksOwner && this.userId !== tasksOwner) return;
+
+    Tasks.update(taskId, { $set: { checked: setChecked } });
+  },
+});
