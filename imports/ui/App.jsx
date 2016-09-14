08039ebe63b7c10f69b5b3f14b3efc3759b72358
Miroslav Martynov <marty.mircha@gmail.com>
2016-09-14 16:53:19 +0300

    Tutorial hide completed tasks, show uncompleted tasks number

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -10,6 +10,14 @@ import Task from './Task';
 
 // App component - represents the whole app
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hideCompleted: false,
+    };
+  }
+
   getTasks() {
     return [
       { _id: 1, text: 'This is task 1' },
@@ -18,8 +26,18 @@ class App extends Component {
     ];
   }
 
+  toggleHideCompleted() {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted,
+    });
+  }
+
   renderTasks() {
-    return this.props.tasks.map((task) => (
+    let filteredTasks = this.props.tasks;
+    if (this.state.hideCompleted) {
+      filteredTasks = filteredTasks.filter(task => !task.checked);
+    }
+    return filteredTasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
@@ -44,19 +62,28 @@ class App extends Component {
     return (
       <div className={mainStyles.container}>
         <header>
-          <h1>Todo List</h1>
+          <h1>Todo List ({this.props.incompleteCount})</h1>
+          <label className={mainStyles['hide-completed']}>
+            <input
+              type="checkbox"
+              readOnly
+              checked={this.state.hideCompleted}
+              onClick={this.toggleHideCompleted.bind(this)}
+            />
+            Hide Completed Tasks
+          </label>
+          {/*
+          * Move this to separate component
+          **/}
+          <form className={mainStyles['new-task']} onSubmit={this.handleSubmit.bind(this)} >
+            <input
+              type="text"
+              ref="textInput"
+              placeholder="Type to add new tasks"
+            />
+          </form>
         </header>
 
-        {/*
-        * Move this to separate component
-        **/}
-        <form className={mainStyles['new-task']} onSubmit={this.handleSubmit.bind(this)} >
-          <input
-            type="text"
-            ref="textInput"
-            placeholder="Type to add new tasks"
-          />
-        </form>
 
         <ul className={todoStyles['tasks-list']}>
           {this.renderTasks()}
@@ -68,10 +95,12 @@ class App extends Component {
 
 App.propTypes = {
   tasks: PropTypes.array.isRequired,
+  incompleteCount: PropTypes.number.isRequired,
 };
 
 export default createContainer(() => {
   return {
     tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+    incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
   };
 }, App);
